47d55170d0647e1c9e48fbe4091895bf771ff7c7
Fabricio Bezerra <fabricio.bezerra@campuscode.com.br>
2018-06-05 15:42:50 -0300

    Installing Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  devise_for :users
+
   resources :farms, only: %w[show new create] do
     resources :harvests, only: %w[show new create]
   end
